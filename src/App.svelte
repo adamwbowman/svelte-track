f0bb103c56accda4a02df0a6531867a178e06234
Adam Bowman <adamwbowman@gmail.com>
2022-01-05 22:05:21 -0600

    rough css grid

--- src/App.svelte
+++ src/App.svelte
@@ -56,23 +56,45 @@
 		}
 		return subTotal;
 	}
+
+	// let editing = false;
+	// function edit() {
+  //   editing = true
+  // }
 	</script>
 
-<main>
-	<p>Total: {Total}</p>
-	<ul>
+<main class="container">
+	<div class="div1">Total: {Total}</div>
 		{#each expenses as expense}
-			<li>{expense.location} || ${expense.amount} || {expense.note} || ${getSubTotal(expense.amount)}
-			<button on:click="{deleteExpense(expense.id)}">x</button>
-			</li>
-		{/each}
-	</ul>
-	<input type="text" placeholder="Location" bind:value="{newLocation}" />
-	<input type="text" placeholder="Amount" bind:value="{newAmount}" />
-	<input type="text" placeholder="Note" bind:value="{newNote}" />
-	<button on:click="{addExpense}">Add</button>
+		<div class="parent">
+
+		<div class="div2">{expense.location}</div>
+			<div class="div3">(${expense.amount})</div>
+			<div class="div4">{expense.note}</div>
+			<div class="div5">${getSubTotal(expense.amount)}<button on:click="{deleteExpense(expense.id)}">x</div>
+			</div>
+
+				{/each}
+	<div class="parent">
+		<div class="div2"><input type="text" placeholder="Location" bind:value="{newLocation}" /></div>
+		<div class="div3"><input type="text" placeholder="Amount" bind:value="{newAmount}" /></div>
+		<div class="div4"><input type="text" placeholder="Note" bind:value="{newNote}" />
+		<button on:click="{addExpense}">Add</button></div>
+	</div>
+	<div class="div5"></div>
 </main>
 
 <style>
-
+.parent { 
+	display: grid; 
+	grid-template-columns: repeat(4, 1fr); 
+	grid-template-rows: repeat(2, 1fr); 
+	grid-column-gap: 10px;
+	grid-row-gap: 10px; 
+}
+.div1 { grid-area: 1 / 1 / 2 / 5; } 
+.div2 { grid-area: 2 / 1 / 3 / 2; } 
+.div3 { grid-area: 2 / 2 / 3 / 3; } 
+.div4 { grid-area: 2 / 3 / 3 / 4; } 
+.div5 { grid-area: 2 / 4 / 3 / 5; } 
 </style>
