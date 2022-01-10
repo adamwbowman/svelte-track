d7e244c8987d6194c6be501513aab52e87d99066
Adam Bowman <adamwbowman@gmail.com>
2022-01-10 12:13:46 -0600

    converted to table

--- src/App.svelte
+++ src/App.svelte
@@ -3,11 +3,14 @@
 	import { db } from './firebase.js'
 	import { collection, query, orderBy, onSnapshot, addDoc, doc, deleteDoc } from "firebase/firestore"; 
 
+	// initial collection
 	let expenses = [];
 
+	// firestore vars
 	const expensesCol = collection(db, 'expenses');
 	const queryAll = query(expensesCol, orderBy("createdAt", "asc"));
 
+	// listener for collection reactivity
 	const listenCol = onSnapshot(queryAll, (querySnapshot) => {
 			expenses = querySnapshot.docs.map(doc => {
 				return { id: doc.id, ...doc.data() }
@@ -15,9 +18,16 @@
 			console.table(expenses);
 	});
 
+
+	// page vars
+	const Total = parseInt(500);
+	let subTotal = parseInt(0);
+
+	// form vas
 	let newLocation = "", newAmount = "", newNote="";
 	let error = "";
 
+	// functions
 	function addExpense() {
 		resetSubTotal();
 		if ((newLocation != "") && (newAmount != "")) {
@@ -45,8 +55,6 @@
 		subTotal = parseInt(0);
 	}
 
-	const Total = parseInt(500);
-	let subTotal = parseInt(0);
 	function getSubTotal(amount) {
 		if (subTotal == 0) {
 			subTotal = (500-amount).toFixed(2);
@@ -61,83 +69,45 @@
 			addExpense();
 		}
 	}
-
-	// let editing = false;
-	// function edit() {
-  //   editing = true
-  // }
 	</script>
 
 <main>
-	<div class="div1">Total: {Total}</div>
-		{#each expenses as expense}
-		<div class="parent">
-			<div class="div2">{expense.location}</div>
-			<div class="div3">$</div>
-			<div class="div4">({expense.amount})</div>
-			<div class="div5">$</div>
-			<div class="div6">{getSubTotal(expense.amount)}</div>
-			<div class="div7"><button class="button button4">{expense.note}</button></div>
-			<div class="div8"><button class="button button4" on:click="{deleteExpense(expense.id)}"><span class="fas fa-trash-alt"></span></button></div>
+	<div class="container">
+		<div class="row">
+				<div class="col">Total: {Total}</div>
 		</div>
-		{/each}
-		<div class="parent">
-			<div class="div2">
-				<input type="text" placeholder="Location" on:keypress="{handleEnter}" bind:value="{newLocation}" size="18" />
-			</div>
-			<div class="div3"></div>
-			<div class="div4">
-				<input type="text" placeholder="$" on:keypress="{handleEnter}" bind:value="{newAmount}" size="5" />
-			</div>
-			<div class="div5"></div>
-			<div class="div6"></div>
-			<div class="div7">
-				<input type="text" placeholder="Note" on:keypress="{handleEnter}" bind:value="{newNote}" size="18" />
-			</div>
-			<div class="div8">
-				<button type="button" class="btn btn-success" on:click="{addExpense}">Add</button>
-			</div>
-		</div>
+		<table class="table table-striped">
+			<thead>
+				<tr>
+					<th scope="col">Location</th>
+					<th scope="col">Amount</th>
+					<th scope="col">SubTotal</th>
+					<th scope="col">Tags</th>
+					<th scope="col">&nbsp;</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each expenses as expense}
+				<tr>
+					<td>{expense.location}</td>
+					<td>({expense.amount})</td>
+					<td>{getSubTotal(expense.amount)}</td>
+					<td><span class="badge rounded-pill bg-info">{expense.note}</span></td>
+					<td><button class="btn btn-secondary btn-sm" on:click="{deleteExpense(expense.id)}"><span class="fas fa-trash-alt"></span></button></td>
+				</tr>
+				{/each}
+				<tr>
+					<td><input type="form-control" on:keypress="{handleEnter}" bind:value="{newLocation}"/></td>
+					<td><input type="form-control" on:keypress="{handleEnter}" bind:value="{newAmount}"/></td>
+					<td>&nbsp;</td>
+					<td><input type="form-control" on:keypress="{handleEnter}" bind:value="{newNote}"/></td>
+					<td><button type="button" class="btn btn-success btn-sm" on:click="{addExpense}">Add</button></td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 </main>
 
 <style>
-.parent { 
-	display: grid; 
-	grid-template-rows: 25px 0px; 
-	grid-template-columns: 200px 0px 100px 0px 100px 200px 0px; 
-	grid-row-gap: 10px; 
-	grid-column-gap: 5px;
-}
-.div1 { grid-area: 1 / 1 / 2 / 8; } 
-.div2 { grid-area: 2 / 1 / 3 / 2; } 
-.div3 { grid-area: 2 / 2 / 3 / 3; } 
-.div4 { 
-	grid-area: 2 / 3 / 3 / 4; 
-	text-align: right;
-} 
-.div5 { 
-	grid-area: 2 / 4 / 3 / 5; 
-	font-weight: bold;
-	font-style: italic;
-} 
-.div6 { 
-	grid-area: 2 / 5 / 3 / 6;
-	text-align: right; 
-	font-weight: bold;
-	font-style: italic;
-}
-.div7 { grid-area: 2 / 6 / 3 / 7; }
-.div8 { grid-area: 2 / 7 / 3 / 8; } 
-.button {
-  background-color: lightgray;
-  border: 1px solid black;
-  color: black;
-  padding: 4px 10px 4px 10px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 12px;
-  margin: 0px 0px;
-}
-.button4 {border-radius: 4px;}
+
 </style>
