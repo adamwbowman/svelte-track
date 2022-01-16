eaee5cd88c4093bc1db3e721a9a4a04f3312811a
Adam Bowman <adamwbowman@gmail.com>
2022-01-16 16:40:47 -0600

    wire up tags

--- src/App.svelte
+++ src/App.svelte
@@ -23,21 +23,25 @@
 	let subTotal = parseInt(0);
 
 	// form vas
-	let newLocation = "", newAmount = "", newNote = "";
+	let newLocation = "", newAmount = "", newTag = "";
 	let error = "";
 
 	// functions
 	function addExpense() {
+		let tagInfo = getTagInfo(newTag).split(",");
+		let newTagName= tagInfo[0];
+		let newTagColor = tagInfo[1];
 		resetSubTotal();
 		if ((newLocation != "") && (newAmount != "")) {
 			//expenses = [...expenses, {location: newLocation}];
 			const docRef = addDoc(expensesCol, {
 				location: newLocation,
 				amount: newAmount,
-				note: newNote,
+				tag: newTagName,
+				tagColor: newTagColor,
 				createdAt: new Date()
 			});
-			newLocation = "", newAmount = "", newNote = "";	
+			newLocation = "", newAmount = "", newTag = "";	
 			error = "";
 		} else {
 			error = "Both location and an amount are required."
@@ -47,6 +51,32 @@
 	async function deleteExpense(index) {
 		resetSubTotal();
 		await deleteDoc(doc(db, "expenses", index));
+	}
+
+	function getTagInfo(tagNumber){
+		switch (tagNumber) {
+			case 1:
+				return newTag = "cart,success";
+				break;
+			case 2:
+				return newTag = "logo-amazon,warning";
+				break;
+			case 3:
+				return newTag = "home,primary";
+				break;
+			case 4:
+				return newTag = "restaurant,danger";
+				break;
+			case 5:
+				return newTag = "subway,info";
+				break;
+			case 6:
+				return newTag = "shirt,dark";
+				break;
+			default:
+				return newTag = "null,null";
+				break;
+		}
 	}
 
 	function resetSubTotal(){
@@ -83,37 +113,37 @@
 	<div class="container">
 		<!-- button group -->
 		<div class="row justify-content-center">
-			<div class="col-lg-4 mb-3">
-				<div class="btn-group d-flex" role="group" aria-label="Tag Button Group">
+			<div class="col-lg-4 mb-4">
+				<div class="btn-group d-flex text-right" role="group" aria-label="Tag Button Group">
 					<!-- grocery button -->
-					<input type="radio" class="btn-check" name="btncheck" id="btncheckGrocery" autocomplete="off" checked/>
-					<label class="btn btn-outline-secondary" for="btncheckGrocery"><ion-icon name="cart"></ion-icon></label>
+					<input type="radio" class="btn-check" name="btnradio" value={1} bind:group={newTag} id="btnradioGrocery" autocomplete="off" checked>
+					<label class="btn btn-outline-secondary" for="btnradioGrocery"><ion-icon name="cart"></ion-icon></label>
 					<!-- amazon button -->
-					<input type="radio" class="btn-check" name="btncheck" id="btncheckAmazon" autocomplete="off"/>
-					<label class="btn btn-outline-secondary" for="btncheckAmazon"><ion-icon name="logo-amazon"></ion-icon></label>
+					<input type="radio" class="btn-check" name="btnradio" value={2} bind:group={newTag} id="btnradioAmazon" autocomplete="off">
+					<label class="btn btn-outline-secondary" for="btnradioAmazon"><ion-icon name="logo-amazon"></ion-icon></label>
 					<!-- home button -->
-					<input type="radio" class="btn-check" name="btncheck" id="btncheckHome" autocomplete="off"/>
-					<label class="btn btn-outline-secondary" for="btncheckHome"><ion-icon name="home"></ion-icon></label>
+					<input type="radio" class="btn-check" name="btnradio" value={3} bind:group={newTag} id="btnradioHome" autocomplete="off">
+					<label class="btn btn-outline-secondary" for="btnradioHome"><ion-icon name="home"></ion-icon></label>
 					<!-- restaurant button -->
-					<input type="radio" class="btn-check" name="btncheck" id="btncheckRestaurant" autocomplete="off"/>
-					<label class="btn btn-outline-secondary" for="btncheckRestaurant"><ion-icon name="restaurant"></ion-icon></label>
+					<input type="radio" class="btn-check" name="btnradio" value={4} bind:group={newTag} id="btnradioRestaurant" autocomplete="off">
+					<label class="btn btn-outline-secondary" for="btnradioRestaurant"><ion-icon name="restaurant"></ion-icon></label>
 					<!-- transit button -->
-					<input type="radio" class="btn-check" name="btncheck" id="btncheckTransit" autocomplete="off"/>
-					<label class="btn btn-outline-secondary" for="btncheckTransit"><ion-icon name="subway"></ion-icon></label>
+					<input type="radio" class="btn-check" name="btnradio" value={5} bind:group={newTag} id="btnradioTransit" autocomplete="off">
+					<label class="btn btn-outline-secondary" for="btnradioTransit"><ion-icon name="subway"></ion-icon></label>
 					<!-- clothes button -->
-					<input type="radio" class="btn-check" name="btncheck" id="btncheckClothes" autocomplete="off"/>
-					<label class="btn btn-outline-secondary" for="btncheckClothes"><ion-icon name="shirt"></ion-icon></label>
+					<input type="radio" class="btn-check" name="btnradio" value={6} bind:group={newTag} id="btnradioClothes" autocomplete="off">
+					<label class="btn btn-outline-secondary" for="btnradioClothes"><ion-icon name="shirt"></ion-icon></label>
 				</div>
 			</div>
 			<!-- location -->
-			<div class="col-md-4 col-lg-4 mb-3">
+			<div class="col-md-4 col-lg-4 mb-4">
 					<input type="type" class="form-control" id="exampleLocation" placeholder="Location" 
 						on:keypress="{handleEnter}" 
 						bind:value="{newLocation}" 
 						use:init/>
 			</div>
 			<!-- amount -->
-			<div class="col-md-3 col-lg-2 mb-3">
+			<div class="col-md-3 col-lg-2 mb-4">
 				<div class="input-group">
 					<span class="input-group-text">$</span>
 					<input type="number" class="form-control" id="exampleAmount" placeholder="Amount" 
@@ -123,31 +153,33 @@
 			</div>
 			<!-- add button -->
 			<div class="col-md-2 mb-5">
-				<button type="button" class="btn btn-success" style="width: 100%">Add</button>
+				<button type="button" class="btn btn-success" style="width: 100%"
+					on:click="{addExpense}"
+				>Add</button>
 			</div>
 		</div>
 		<!-- journal -->
 		{#each expenses as expense}
-		<div class="row gx-3 border">
-			<div class="col-1 border col-lg-3"></div>
+		<div class="row gx-3">
+			<div class="col-1 col-lg-3"></div>
 			<!-- tag -->
-			<div class="col-1  pull-left border">
+			<div class="col-1 pull-left">
 					<button type="button" class="btn btn-{expense.tagColor} btn-sm">
 							<ion-icon name="{expense.tag}"></ion-icon>
 					</button>
 				</div>
-			<div class="col-5 border col-lg-3">
+			<div class="col-5 col-lg-3">
 				<p class="ps-2 p-md-0">{expense.location}</p>
 			</div>
-			<div class="col-2 border col-lg-1 overflow-auto">
+			<div class="col-2 col-lg-1 overflow-auto">
 				<p class="text-end">-{expense.amount}</p>
 			</div>
-			<div class="col-2 border col-lg-1 overflow-auto">
+			<div class="col-2 col-lg-1 overflow-auto">
 				<p class="text-secondary text-end">{getSubTotal(expense.amount)}</p>
 			</div>
 			<!-- delete button -->
-			<div class="col-1 border d-none d-md-block">
-				<button type="button" class="btn btn-secondary btn-sm" 
+			<div class="col-1 d-none d-md-block">
+				<button type="button" class="btn btn-outline-secondary btn-sm" 
 					on:click="{deleteExpense(expense.id)}">
 						<ion-icon name="trash"></ion-icon>
 				</button>
