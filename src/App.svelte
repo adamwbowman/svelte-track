990be89015ac193f78a03de9c83c04689d5e0656
Adam Bowman <adamwbowman@gmail.com>
2022-01-20 10:37:29 -0600

    added short and verbose for dates

--- src/App.svelte
+++ src/App.svelte
@@ -6,172 +6,171 @@
 	import { fade, fly } from 'svelte/transition';
 	import {formatDay, formatMonth} from './utils.js';
 
-	// initial collections
-	let expenses = [];
-	let filteredExpenses = [];
-	let expensesByTag = [];
+	// future use 
+	let formattedStartDate = " ";
+	let formattedEndDate = " ";
+
+	// collections vars
+		let expenses = [];
+		let filteredExpenses = [];
+		let expensesByTag = [];
 
 	// date processing
-	let today = new Date();
-	let month = (today.getMonth() +1);
-	let day = (today.getDate());
-	let year = (today.getFullYear());
-	if (month.toString().length < 2) { month = '0' + month; }
-	if (day.toString().length < 2) { day = '0' + day; }
-	let dateToday = [month, day, year].join('/')
-	let findWeek = weeks.find(el => (el.start > dateToday));
-	let currentWeek = weeks.find(el => (el.week == (findWeek.week -1)));
-console.log(findWeek);
-	let startDate = new Date(currentWeek.start);
-	let endDate = new Date(currentWeek.end);
-console.log(startDate+"-"+endDate);
+		let today = new Date();
+		let month = (today.getMonth() +1);
+		let day = (today.getDate());
+		let year = (today.getFullYear());
+		if (month.toString().length < 2) { month = '0' + month; }
+		if (day.toString().length < 2) { day = '0' + day; }
+		let dateToday = [month, day, year].join('/')
+		let findWeek = weeks.find(el => (el.start > dateToday));
+		// console.log(findWeek);
+		let currentWeek = weeks.find(el => (el.week == (findWeek.week -1)));
+		let startDate = new Date(currentWeek.start);
+		let endDate = new Date(currentWeek.end);
+		console.log(startDate+"-"+endDate);
 
-	// firestore vars
-	const expensesCol = collection(db, 'expenses');
-	const queryAll = query(expensesCol,
-		orderBy("createdAt", "asc")
-	);
+	// firestore entire get collection
+		const expensesCol = collection(db, 'expenses');
+		const queryAll = query(expensesCol,
+			orderBy("createdAt", "asc")
+		);
 
 	// listener for collection reactivity
-	const listenCol = onSnapshot(queryAll, (querySnapshot) => {
-		expenses = querySnapshot.docs.map(doc => {
-			return { id: doc.id, ...doc.data() }
+		const listenCol = onSnapshot(queryAll, (querySnapshot) => {
+			expenses = querySnapshot.docs.map(doc => {
+				return { id: doc.id, ...doc.data() }
+			});
+			filteredExpenses = expenses;
+			filterExpenses(1);
 		});
-		filteredExpenses = expenses;
-		filterExpenses(1);
-	});
 
-
-
-
-let formattedStartDate = " ";
-let formattedEndDate = " ";
-
-	function filterExpenses(weekNum) {
-		resetSubTotal();
-		var selectedWeek = weeks.find(el => (el.week == (findWeek.week - parseInt(weekNum))));
-// console.log(selectedWeek);
-		filteredExpenses = expenses.filter(el => {
-			var dbDate = el.createdAt.toDate();
-			startDate = new Date(selectedWeek.start);
-			endDate = new Date(selectedWeek.end);
-			return (dbDate >= startDate && dbDate <= endDate);
-		});
-// console.table(filteredExpenses);
-// console.table(expenses);
-	}
+		// filter collection array
+		function filterExpenses(weekNum) {
+			resetSubTotal();
+			var selectedWeek = weeks.find(el => (el.week == (findWeek.week - parseInt(weekNum))));
+			// console.log(selectedWeek);
+			filteredExpenses = expenses.filter(el => {
+				var dbDate = el.createdAt.toDate();
+				startDate = new Date(selectedWeek.start);
+				endDate = new Date(selectedWeek.end);
+				return (dbDate >= startDate && dbDate <= endDate);
+			});
+			// console.table(filteredExpenses);
+			// console.table(expenses);
+		}
 
 
 
 
 
 	// page vars
-	const Total = parseInt(500).toFixed(2);
-	let subTotal = parseInt(0);
-	let currentRange = "current";
+		const Total = parseInt(500).toFixed(2);
+		let subTotal = parseInt(0);
+		let currentRange = "current";
 
 	// form vars
-	let bigTagName = "", bigTagColor = "", bigTagLabel = "";
-	let newLocation = "", newAmount = "", newTag = "";
-	let error = "";
+		let bigTagName = "", bigTagColor = "", bigTagLabel = "";
+		let newLocation = "", newAmount = "", newTag = "";
+		let error = "";
 
 	// filtering functions
-	function setTag(tagName, tagColor) {
-		expensesByTag = expenses.filter(el => el.tag === tagName);
-		bigTagName = tagName;
-		bigTagColor = tagColor;
-		switch (tagName) {
-			case "cart":
-				return bigTagLabel = "Groceries"; break;
-			case "logo-amazon":
-				return bigTagLabel = "Amazon"; break;
-			case "home":
-				return bigTagLabel = "Home Goods"; break;
-			case "restaurant":
-				return bigTagLabel = "Eating Out"; break;
-			case "subway":
-				return bigTagLabel = "Transit"; break;
-			case "shirt":
-				return bigTagLabel = "Clothes"; break;
-			default:
-				return bigTagLabel = "Error"; break;
-		}
-	}
-
-	// crud functions
-	function addExpense() {
-		let tagInfo = getTagInfo(newTag).split(",");
-		let newTagName= tagInfo[0];
-		let newTagColor = tagInfo[1];
-		resetSubTotal();
-		if ((newLocation != "") && (newAmount != "")) {
-			//expenses = [...expenses, {location: newLocation}];
-			var strMonth = new Date().getMonth();
-			var strDay = new Date().getDay();
-			strMonth = formatMonth(strMonth);
-			const docRef = addDoc(expensesCol, {
-				location: newLocation,
-				amount: newAmount,
-				tag: newTagName,
-				tagColor: newTagColor,
-				createdAt: new Date(),
-				year: new Date().getFullYear(),
-				month: strMonth,
-				day: new Date().getDay(),
-				date: new Date().getDate()
-			});
-			newLocation = "", newAmount = "", newTagName = "", newTagColor = "";	
-			error = "";
-		} else {
-			if (newTagName == "error") {
-				return error = "A tag is required."
-			} else {
-				return error = "Both location and an amount are required."
+		function setTag(tagName, tagColor) {
+			expensesByTag = expenses.filter(el => el.tag === tagName);
+			bigTagName = tagName;
+			bigTagColor = tagColor;
+			switch (tagName) {
+				case "cart":
+					return bigTagLabel = "Groceries"; break;
+				case "logo-amazon":
+					return bigTagLabel = "Amazon"; break;
+				case "home":
+					return bigTagLabel = "Home Goods"; break;
+				case "restaurant":
+					return bigTagLabel = "Eating Out"; break;
+				case "subway":
+					return bigTagLabel = "Transit"; break;
+				case "shirt":
+					return bigTagLabel = "Clothes"; break;
+				default:
+					return bigTagLabel = "Error"; break;
 			}
 		}
-	}
 
-	async function deleteExpense(index) {
-		resetSubTotal();
-		await deleteDoc(doc(db, "expenses", index));
-	}
-
-	function getTagInfo(tagNumber){
-		switch (tagNumber) {
-			case 1:
-				return newTag = "cart,success"; break;
-			case 2:
-				return newTag = "logo-amazon,warning"; break;
-			case 3:
-				return newTag = "home,primary"; break;
-			case 4:
-				return newTag = "restaurant,danger"; break;
-			case 5:
-				return newTag = "subway,info"; break;
-			case 6:
-				return newTag = "shirt,dark"; break;
-			default:
-				return newTag = "error,error"; break;
+	// crud functions
+		function addExpense() {
+			let tagInfo = getTagInfo(newTag).split(",");
+			let newTagName= tagInfo[0];
+			let newTagColor = tagInfo[1];
+			resetSubTotal();
+			if ((newLocation != "") && (newAmount != "")) {
+				//expenses = [...expenses, {location: newLocation}];
+				var strDay = new Date().getDay();
+				strDay = formatDay(strDay);
+				var strDayShort = strDay.substring(0, 3);
+				var strMonth = new Date().getMonth();
+				var strMonthVerbose = formatMonth(strMonth);
+				var strMonthShort = strMonthVerbose.substring(0, 3);
+				const docRef = addDoc(expensesCol, {
+					amount: newAmount,
+					date: new Date().getDate(),
+					day: strDay,
+					dayShort: strDayShort,
+					location: newLocation,
+					month: (strMonth+1),
+					monthShort: strMonthShort,
+					monthVerbose: strMonthVerbose,
+					tag: newTagName,
+					tagColor: newTagColor,
+					year: new Date().getFullYear(),
+					createdAt: new Date()
+				});
+				newLocation = "", newAmount = "", newTagName = "", newTagColor = "";	
+				error = "";
+			} else {
+				if (newTagName == "error") {
+					return error = "A tag is required."
+				} else {
+					return error = "Both location and an amount are required."
+				}
+			}
 		}
-	}
+
+		async function deleteExpense(index) {
+			resetSubTotal();
+			await deleteDoc(doc(db, "expenses", index));
+		}
+
+		function getTagInfo(tagNumber){
+			switch (tagNumber) {
+				case 1: return newTag = "cart,success"; break;
+				case 2: return newTag = "logo-amazon,warning"; break;
+				case 3: return newTag = "home,primary"; break;
+				case 4: return newTag = "restaurant,danger"; break;
+				case 5: return newTag = "subway,info"; break;
+				case 6: return newTag = "shirt,dark"; break;
+				default: return newTag = "error,error"; break;
+			}
+		}
 
 	// page functions
-	function getSubTotal(amount) {
-		if (subTotal == 0) {
-			subTotal = (500-amount).toFixed(2);
-		} else {
-			subTotal = (subTotal-amount).toFixed(2);
+		function getSubTotal(amount) {
+			if (subTotal == 0) {
+				subTotal = (500-amount).toFixed(2);
+			} else {
+				subTotal = (subTotal-amount).toFixed(2);
+			}
+			return subTotal;
 		}
-		return subTotal;
-	}
 
-	function handleEnter(event) { if (event.key == "Enter") { addExpense(); }}
-	function resetSubTotal(){ subTotal = parseInt(0); }
-	function resetError() { return error = ""; }
-	function init(el) { el.focus(); }
+		function handleEnter(event) { if (event.key == "Enter") { addExpense(); }}
+		function resetSubTotal(){ subTotal = parseInt(0); }
+		function resetError() { return error = ""; }
+		function init(el) { el.focus(); }
 </script>
 
 <main>
+<!-- navbar -->
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 		<div class="container-fluid">
 			<a class="navbar-brand" href="/#">${Total}</a>
@@ -273,7 +272,7 @@ let formattedEndDate = " ";
 						</button>
 					</div>
 				<div class="col-5 col-lg-3">
-					<p class="ps-2 p-md-0">{new Date(expense.createdAt * 1000).getDate()} - {expense.location}</p>
+					<p class="ps-2 p-md-0">{expense.dayShort}: {expense.location}</p>
 				</div>
 				<div class="col-2 col-lg-1 overflow-auto">
 					<p class="text-end">-{expense.amount}</p>
@@ -305,11 +304,11 @@ let formattedEndDate = " ";
 		</div>
 		<div class="offcanvas-body">
 			<div class="container">
-<!-- list -->
+	<!-- list -->
 				{#each expensesByTag as expense}
 					<div class="row gx-2" in:fly="{{ x: -200, duration: 2000 }}">
 						<div class="col-2">
-							<p class="text-start">{expense.month}/{expense.day}</p>
+							<p class="text-start">{expense.month}.{expense.date}</p>
 						</div>
 						<div class="col-8">
 							<p class="text-start">{expense.location}</p>
--- src/utils.js
+++ src/utils.js
@@ -1,36 +1,33 @@
 
 export function formatDay(day) {
-
+	var vDay = "";
+	switch (day) {
+		case 0: return vDay = "Sunday"; break;
+		case 1: return vDay = "Monday"; break;
+		case 2: return vDay = "Tuesday"; break;
+		case 3: return vDay = "Wednesday"; break;
+		case 4: return vDay = "Thursday"; break;
+		case 5: return vDay = "Friday"; break;
+		case 6: return vDay = "Saturday"; break;
+		default: return vDay = "Error"; break;
+	}
 }
 
 export function formatMonth(month) {
 	var vMonth = "";
 	switch (month) {
-		case 0:
-			return vMonth = "January"; break;
-		case 1:
-			return vMonth = "February"; break;
-		case 2:
-			return vMonth = "March"; break;
-		case 3:
-			return vMonth = "April"; break;
-		case 4:
-			return vMonth = "May"; break;
-		case 5:
-			return vMonth = "June"; break;
-		case 6:
-			return vMonth = "July"; break;
-		case 7:
-			return vMonth = "August"; break;
-		case 8:
-			return vMonth = "September"; break;
-		case 9:
-			return vMonth = "October"; break;
-		case 10:
-			return vMonth = "November"; break;
-		case 11:
-			return vMonth = "December"; break;
-		default:
-			return vMonth = "Error"; break;
+		case 0: return vMonth = "January"; break;
+		case 1: return vMonth = "February"; break;
+		case 2: return vMonth = "March"; break;
+		case 3: return vMonth = "April"; break;
+		case 4: return vMonth = "May"; break;
+		case 5: return vMonth = "June"; break;
+		case 6: return vMonth = "July"; break;
+		case 7: return vMonth = "August"; break;
+		case 8: return vMonth = "September"; break;
+		case 9: return vMonth = "October"; break;
+		case 10: return vMonth = "November"; break;
+		case 11: return vMonth = "December"; break;
+		default: return vMonth = "Error"; break;
 	}
 }
